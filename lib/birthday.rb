3971c72fc28bc73c72b875eb1b34ef5e18fffda4
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-16 01:34:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,4 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |kid_name, age|
+    puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
+  end
 end
